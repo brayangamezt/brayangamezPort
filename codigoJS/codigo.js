77333d1498f5329d97af669e836c3c76addb7c93
brayangamezt <bgamezt@outlook.com>
2021-10-30 17:26:57 -0700

    agregando portafolio

---
+++ codigoJS/codigo.js
@@ -0,0 +1,14 @@
+'use strict'
+/* 
+const boton=$('#boton');
+boton.click(function(){
+	$('#panel').slideDown(1000);
+});
+
+const btn2=$('#arriba');
+btn2.click(function(){
+	$('#panel').slideUp(1000),()=>{
+		$('#panel').css("display", "none");
+	};
+});
+*/
